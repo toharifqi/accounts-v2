ceb43266f8395405ad5abb6fd0f58329893cf98f
Rifqi Naufal Tohari <rifqinaufaltohari@gmail.com>
2024-04-22 13:20:17 +0700

    change h2 database into mysql and remove cloud bus dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,10 +36,9 @@ dependencies {
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
-	implementation("org.springframework.cloud:spring-cloud-starter-bus-amqp:4.1.0")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("com.h2database:h2")
+	runtimeOnly("com.mysql:mysql-connector-j")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
